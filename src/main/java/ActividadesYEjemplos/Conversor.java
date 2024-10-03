925dac772710d3dddd9b7c7c4907804eddfb8960
brayan8abo <brayan8abo@outlook.es>
2024-10-03 17:16:14 +0200

    .

--- src/main/java/ActividadesYEjemplos/Conversor.java
+++ src/main/java/ActividadesYEjemplos/Conversor.java
@@ -6,14 +6,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
-import java.awt.Window.Type;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.Color;
 import javax.swing.border.LineBorder;
-import java.awt.SystemColor;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
