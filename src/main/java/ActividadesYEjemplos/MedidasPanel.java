9dd802c0d81973756047e816046a7949e7edd718
Brayan8abo <brayan8abo@outlook.es>
2024-10-06 18:41:20 +0200

    update

--- src/main/java/ActividadesYEjemplos/MedidasPanel.java
+++ src/main/java/ActividadesYEjemplos/MedidasPanel.java
@@ -22,13 +22,11 @@ public class MedidasPanel extends JFrame {
 
 	protected static final JLabel textField = null;
 	private JPanel contentPane;
-	private JTextField textDato;
 
-	/**
-	 * Create the frame.
-	 */
+	JTextField textDatoIngresado = new JTextField();
+	JLabel lblFondo, lblResultado;
+
 	public MedidasPanel() {
-		setResizable(false);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setTitle("Conversor de Medidas");
 		setBounds(100, 100, 590, 360);
@@ -36,74 +34,101 @@ public class MedidasPanel extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		setLocationRelativeTo(null);
-
-		JButton btnConvertir = new JButton("Convertir");
-		btnConvertir.setBounds(233, 280, 100, 30);
-		btnConvertir.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				// Aquí iría la lógica de conversión.
-				String valor = textField.getText();
-				System.out.println("Valor a convertir: " + valor);
-
-			}
-		});
 		contentPane.setLayout(null);
-		contentPane.add(btnConvertir);
 
 		JComboBox comboBox = new JComboBox();
+		comboBox.setBackground(Color.LIGHT_GRAY);
 		comboBox.addActionListener(new ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				String opcion = comboBox.getSelectedItem().toString();
-				if (opcion == comboBox.getName()) {
-					convertirCentimetrosAMetros(100);
-					
+				double dato1, resultado;
+				dato1 = Double.parseDouble(textDatoIngresado.getText());
+				String option = (String) comboBox.getSelectedItem();
+
+				if (option.equals("Centimetros")) {
+					resultado = dato1 * 100;
+					lblResultado.setText("" + resultado + " cm");
 				}
-				
+				if (option.equals("Metros")) {
+					resultado = dato1;
+					lblResultado.setText("" + resultado + " m");
+				}
+				if (option.equals("Kilometros")) {
+					resultado = dato1 / 1000;
+					lblResultado.setText("" + resultado + " km");
+				}
 
 			}
 		});
-		comboBox.setBounds(32, 132, 137, 40);
+
+		JButton btnSalir = new JButton("SALIR");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+
+		lblResultado = new JLabel("");
+		lblResultado.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblResultado.setFont(new Font("Mongolian Baiti", Font.BOLD, 16));
+		lblResultado.setBounds(419, 189, 105, 38);
+		contentPane.add(lblResultado);
+
+		btnSalir.setBackground(Color.WHITE);
+		btnSalir.setFont(new Font("Mongolian Baiti", Font.BOLD, 16));
+		btnSalir.setBounds(250, 263, 89, 23);
+		contentPane.add(btnSalir);
+
+		JButton btnConvetir = new JButton("CONVERTIR");
+		btnConvetir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				double dato1, resultado;
+				dato1 = Double.parseDouble(textDatoIngresado.getText());
+				String option = (String) comboBox.getSelectedItem();
+
+				if (option.equals("Centimetros")) {
+					resultado = dato1 * 100;
+					lblResultado.setText("" + resultado + " cm");
+				}
+				if (option.equals("Metros")) {
+					resultado = dato1;
+					lblResultado.setText("" + resultado + " m");
+				}
+				if (option.equals("Kilometros")) {
+					resultado = dato1 / 1000;
+					lblResultado.setText("" + resultado + " km");
+				}
+
+			}
+		});
+
+		btnConvetir.setFont(new Font("Mongolian Baiti", Font.PLAIN, 11));
+		btnConvetir.setBounds(419, 138, 103, 40);
+		contentPane.add(btnConvetir);
+
+		JLabel lblTexto = new JLabel("ELIGE A QUIERES CONVERTIR");
+		lblTexto.setForeground(Color.RED);
+		lblTexto.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTexto.setFont(new Font("Mongolian Baiti", Font.BOLD | Font.ITALIC, 18));
+		lblTexto.setBounds(120, 37, 320, 68);
+		contentPane.add(lblTexto);
+		comboBox.setBounds(36, 137, 134, 40);
 
 		comboBox.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
-		comboBox.setModel(new DefaultComboBoxModel(new String[] { "CENTIMETROS", "METROS", "KILOMETROS" }));
-		comboBox.setMaximumRowCount(3);
+		comboBox.setModel(
+				new DefaultComboBoxModel(new String[] { "ESCOGE UNO", "CENTIMETROS", "METROS", "KILOMETROS" }));
+		comboBox.setMaximumRowCount(4);
 		contentPane.add(comboBox);
 
-		JLabel lblOpcion = new JLabel("ESCOGE UNA OPCÓN");
-		lblOpcion.setBounds(163, 32, 263, 89);
-		lblOpcion.setForeground(new Color(128, 0, 0));
-		lblOpcion.setHorizontalAlignment(SwingConstants.CENTER);
-		lblOpcion.setFont(new Font("Mongolian Baiti", Font.PLAIN, 24));
-		contentPane.add(lblOpcion);
+		textDatoIngresado = new JTextField();
+		textDatoIngresado.setHorizontalAlignment(SwingConstants.CENTER);
+		textDatoIngresado.setBackground(Color.LIGHT_GRAY);
+		textDatoIngresado.setBounds(236, 137, 103, 40);
+		contentPane.add(textDatoIngresado);
+		textDatoIngresado.setColumns(10);
 
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(393, 132, 137, 40);
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] { "CENTIMETROS", "METROS", "KILOMETROS" }));
-		comboBox_1.setMaximumRowCount(3);
-		comboBox_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 15));
-		contentPane.add(comboBox_1);
-
-		textDato = new JTextField();
-		textDato.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				textDato.getText();
-			}
-		});
-		textDato.setBounds(233, 142, 86, 20);
-		contentPane.add(textDato);
-		textDato.setColumns(10);
-
-		JLabel lblFondo = new JLabel("");
+		lblFondo = new JLabel("");
 		lblFondo.setBounds(0, 0, 574, 321);
 		lblFondo.setIcon(new ImageIcon(MedidasPanel.class.getResource("/img/fondo.png")));
 		contentPane.add(lblFondo);
-	}
-
-	public static double convertirCentimetrosAMetros(double centimetros) {
-		return centimetros / 100;
-	}
-
-	public static double convertirMetrosAKilometros(double metros) {
-		return metros / 1000;
 	}
 }
