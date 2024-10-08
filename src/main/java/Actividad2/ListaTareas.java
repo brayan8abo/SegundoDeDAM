35e2fe9296e8aae74f7c9a252c082da85051d187
Brayan8abo <brayan8abo@outlook.es>
2024-10-09 01:25:31 +0200

    Terminacion y detallado por completar, 9/10/2024 1.25AM

--- src/main/java/Actividad2/ListaTareas.java
+++ src/main/java/Actividad2/ListaTareas.java
@@ -15,13 +15,17 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import java.awt.Dialog;
 import java.awt.SystemColor;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JSeparator;
 
 public class ListaTareas extends JFrame {
 
 	private JPanel contentPane;
 	private static int seleccionado;
-	JList lista = new JList();
+	JList<String> lista = new JList();
 	String nuevaTarea;
 
 	DefaultListModel<String> listModel = new DefaultListModel<>();
@@ -51,20 +55,21 @@ public class ListaTareas extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 570, 410);
 		contentPane = new JPanel();
-		contentPane.setBackground(SystemColor.activeCaption);
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		setLocationRelativeTo(null);
 
-		lista = new JList();
+		lista = new JList<String>();
+		lista.setBackground(SystemColor.activeCaption);
 		lista.setModel(listModel);
-		lista.setBounds(121, 53, 333, 145);
+		lista.setBounds(129, 108, 333, 145);
 		contentPane.add(lista);
 
 		JButton btnAgregar = new JButton("AGREGAR");
 		btnAgregar.setBackground(new Color(255, 255, 255));
-		JFrame dialog = this;
+		// JFrame dialog = this;
 		btnAgregar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
@@ -76,7 +81,8 @@ public class ListaTareas extends JFrame {
 				if (nuevaTarea != null && !nuevaTarea.isEmpty()) {
 					listModel.addElement(nuevaTarea);
 
-				}
+				} else
+					JOptionPane.showMessageDialog(btnAgregar,"ERROR");
 
 			}
 
@@ -89,7 +95,13 @@ public class ListaTareas extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 
 				int selecionado = lista.getSelectedIndex();
-				listModel.remove(selecionado);
+
+				if (selecionado != -1) {
+					listModel.remove(selecionado);
+				} else
+					JOptionPane.showMessageDialog(btnEliminar, "ERROR");
+				
+
 			}
 		});
 		btnEliminar.setBounds(290, 322, 122, 23);
@@ -104,22 +116,21 @@ public class ListaTareas extends JFrame {
 				lista.getSelectedIndex();
 				if (seleccionado != -1) {
 
-					String tareaActual = listModel.getElementAt(seleccionado);
+					// String tareaActual = listModel.getElementAt(seleccionado);
 
 					Tarea tareaModificar = new Tarea(ListaTareas.this);
-                    tareaModificar.setVisible(true);
-                    
-                    
-                    String tareaModificada = tareaModificar.getTarea();
-                    if (tareaModificada != null && !tareaModificada.isEmpty()) {
-                        // Reemplazamos la tarea en la lista con la tarea modificada
-                        listModel.setElementAt(tareaModificada, seleccionado);
-                    }
-                } else {
+					tareaModificar.setVisible(true);
+
+					String tareaModificada = tareaModificar.getTarea();
+					if (tareaModificada != null && !tareaModificada.isEmpty()) {
+						// Reemplazamos la tarea en la lista con la tarea modificada
+						listModel.setElementAt(tareaModificada, seleccionado);
+					}
+				} else {
 					JOptionPane.showMessageDialog(ListaTareas.this, "ERROR");
-                }
-            }
-        });
+				}
+			}
+		});
 		btnModificar.setBounds(151, 322, 129, 23);
 		contentPane.add(btnModificar);
 
@@ -131,5 +142,10 @@ public class ListaTareas extends JFrame {
 		});
 		btnSalir.setBounds(422, 322, 122, 23);
 		contentPane.add(btnSalir);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(ListaTareas.class.getResource("/img/Diseño sin título.png")));
+		lblNewLabel.setBounds(0, 0, 554, 371);
+		contentPane.add(lblNewLabel);
 	}
 }
--- src/main/java/Actividad2/Tarea.java
+++ src/main/java/Actividad2/Tarea.java
@@ -14,6 +14,8 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import java.awt.SystemColor;
+import java.awt.Color;
+import javax.swing.ImageIcon;
 
 public class Tarea extends JDialog {
 
@@ -24,9 +26,11 @@ public class Tarea extends JDialog {
 
     public Tarea(JFrame padre) {
         super(padre, "Tareas", true);  // El true hace que sea modal
+        setForeground(new Color(255, 0, 0));
         setResizable(false);
         setBounds(100, 100, 450, 300);
         getContentPane().setLayout(new BorderLayout());
+        contentPanel.setBackground(new Color(255, 255, 255));
         contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         getContentPane().add(contentPanel, BorderLayout.CENTER);
         contentPanel.setLayout(null);
@@ -37,24 +41,32 @@ public class Tarea extends JDialog {
         textTarea.setColumns(10);
 
         JLabel lblNewLabel = new JLabel("INGRESA LA TAREA");
-        lblNewLabel.setForeground(SystemColor.textHighlight);
-        lblNewLabel.setFont(new Font("Yu Gothic Medium", Font.BOLD, 18));
+        lblNewLabel.setForeground(SystemColor.inactiveCaptionText);
+        lblNewLabel.setFont(new Font("Microsoft YaHei Light", Font.BOLD | Font.ITALIC, 21));
         lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
         lblNewLabel.setBounds(113, 29, 206, 32);
         contentPanel.add(lblNewLabel);
+        
+        JLabel lblNewLabel_1 = new JLabel("");
+        lblNewLabel_1.setIcon(new ImageIcon(Tarea.class.getResource("/img/Diseño sin título.png")));
+        lblNewLabel_1.setBounds(-80, 0, 514, 272);
+        contentPanel.add(lblNewLabel_1);
         setLocationRelativeTo(null);
 
         JPanel buttonPane = new JPanel();
-        buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        buttonPane.setBackground(new Color(255, 255, 255));
         getContentPane().add(buttonPane, BorderLayout.SOUTH);
 
         JButton okButton = new JButton("AGREGAR");
         okButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                tarea = textTarea.getText();  // Guardamos el texto ingresado
+               
+            	
+            	tarea = textTarea.getText();  // Guardamos el texto ingresado
                 dispose();  // Cerramos el diálogo
             }
         });
+        buttonPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
         buttonPane.add(okButton);
         getRootPane().setDefaultButton(okButton);
 
