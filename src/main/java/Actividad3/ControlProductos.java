2ab0368871bdbd83b02ac19656be758b95edb988
brayan8abo <brayan8abo@outlook.es>
2024-10-14 20:31:06 +0200

    update

--- src/main/java/Actividad3/ControlProductos.java
+++ src/main/java/Actividad3/ControlProductos.java
@@ -3,38 +3,33 @@ package Actividad3;
 import java.util.ArrayList;
 
 public class ControlProductos {
-	ArrayList<Productos> listaProductos = new ArrayList<Productos>();
-	
-	String bebida="Bebida",postre="Postre",comida="Comida";
-	
-	Productos p1 = new Productos("Cerveza", bebida, 3.0);
-	Productos p2 = new Productos("Agua", bebida, 2.0);
-	Productos p3 = new Productos("Refresco", bebida, 3.0);
-	Productos p4 = new Productos("Zumo", bebida, 3.2);
-	Productos p5 = new Productos("Fruta", postre, 2.8);
-	Productos p6 = new Productos("Tarta", postre, 3.0);
-	Productos p7 = new Productos("Helado", postre, 3.0);
-	Productos p8 = new Productos("Racion De Patatas Bravas", comida, 9.0);
-	Productos p9 = new Productos("Bocata Beicon Queso", comida, 4.5);
-	Productos p10 = new Productos("Bocata De Tortilla", comida, 4.0);
-	
-	Listaproductos.add(p1);
-	listaProductos.add(p2);
-	listaProductos.add(p3);
-	listaProductos.add(p4);
-	listaProductos.add(p5);
-	listaProductos.add(p6);
-	listaProductos.add(p7);
-	listaProductos.add(p8);
-	listaProductos.add(p9);
-	listaProductos.add(p10);
-	
-	return productos;
-	
-	
-	
-	
-	
-	
+	public static void main(String[] args) {
+		ArrayList<Productos> listaProductos = new ArrayList<Productos>();
+
+		String bebida = "Bebida", postre = "Postre", comida = "Comida";
+
+		Productos p1 = new Productos("Cerveza", bebida, 3.0);
+		Productos p2 = new Productos("Agua", bebida, 2.0);
+		Productos p3 = new Productos("Refresco", bebida, 3.0);
+		Productos p4 = new Productos("Zumo", bebida, 3.2);
+		Productos p5 = new Productos("Fruta", postre, 2.8);
+		Productos p6 = new Productos("Tarta", postre, 3.0);
+		Productos p7 = new Productos("Helado", postre, 3.0);
+		Productos p8 = new Productos("Racion De Patatas Bravas", comida, 9.0);
+		Productos p9 = new Productos("Bocata Beicon Queso", comida, 4.5);
+		Productos p10 = new Productos("Bocata De Tortilla", comida, 4.0);
+
+		listaProductos.add(p1);
+		listaProductos.add(p2);
+		listaProductos.add(p3);
+		listaProductos.add(p4);
+		listaProductos.add(p5);
+		listaProductos.add(p6);
+		listaProductos.add(p7);
+		listaProductos.add(p8);
+		listaProductos.add(p9);
+		listaProductos.add(p10);
+
+	}
 
 }
