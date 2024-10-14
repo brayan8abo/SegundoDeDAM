bf232b5e1243df6c66ef066085cccb4b89cb9f37
brayan8abo <brayan8abo@outlook.es>
2024-10-14 20:31:40 +0200

    update

--- src/main/java/Actividad3/ControlProductos.java
+++ src/main/java/Actividad3/ControlProductos.java
@@ -8,16 +8,16 @@ public class ControlProductos {
 
 		String bebida = "Bebida", postre = "Postre", comida = "Comida";
 
-		Productos p1 = new Productos("Cerveza", bebida, 3.0);
-		Productos p2 = new Productos("Agua", bebida, 2.0);
-		Productos p3 = new Productos("Refresco", bebida, 3.0);
-		Productos p4 = new Productos("Zumo", bebida, 3.2);
-		Productos p5 = new Productos("Fruta", postre, 2.8);
-		Productos p6 = new Productos("Tarta", postre, 3.0);
-		Productos p7 = new Productos("Helado", postre, 3.0);
-		Productos p8 = new Productos("Racion De Patatas Bravas", comida, 9.0);
-		Productos p9 = new Productos("Bocata Beicon Queso", comida, 4.5);
-		Productos p10 = new Productos("Bocata De Tortilla", comida, 4.0);
+		Productos p1 = new Productos("Agua", bebida, 3.0);
+		Productos p2 = new Productos("Refresco", bebida, 2.0);
+		Productos p3 = new Productos("Cerveza", bebida, 3.0);
+		Productos p4 = new Productos("Jugo", bebida, 3.2);
+		Productos p5 = new Productos("Torta", postre, 2.8);
+		Productos p6 = new Productos("Helado", postre, 3.0);
+		Productos p7 = new Productos("Flan", postre, 3.0);
+		Productos p8 = new Productos("Racion De Alitas", comida, 9.0);
+		Productos p9 = new Productos("Bocadillo De Pollo", comida, 5.0);
+		Productos p10 = new Productos("Bocadillo de Lomo", comida, 4.5);
 
 		listaProductos.add(p1);
 		listaProductos.add(p2);
