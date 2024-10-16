c265849da6d1e9c7097854ec208859104d0a18b4
brayan8abo <brayan8abo@outlook.es>
2024-10-16 20:21:29 +0200

    update

--- src/main/java/Actividad3/TPVview.java
+++ src/main/java/Actividad3/TPVview.java
@@ -5,9 +5,12 @@ import java.awt.GridBagConstraints;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
 import java.awt.GridBagLayout;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class TPVview extends JFrame {
@@ -30,6 +33,7 @@ public class TPVview extends JFrame {
 			}
 		});
 	}
+
 	public void initGridBagLayout(GridBagConstraints gblc, int x, int y, int w, int h, double wx, double wh) {
 
 		gblc.gridx = x;
@@ -47,34 +51,80 @@ public class TPVview extends JFrame {
 		setBounds(100, 100, 700, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		ClassLoader classloader = getClass().getClassLoader();
 
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		contentPane.setLayout(gbl_contentPane);
-	
+
 		GridBagConstraints gblc1 = new GridBagConstraints();
-		initGridBagLayout(gblc1, 0, 0, 2, 1, 0.5, 0.2); // la primera es la x y la segunda y
+		initGridBagLayout(gblc1, 0, 0, 2, 1, 0.2, 0.5);
 		GridBagConstraints gblc2 = new GridBagConstraints();
 		initGridBagLayout(gblc2, 2, 0, 2, 1, 0.2, 0.5);
-		//GridBagConstraints gblc3 = new GridBagConstraints();
-		//initGridBagLayout(gblc3, 0, 1, 1, 1, 0.25, 0.2);
 		GridBagConstraints gblc3 = new GridBagConstraints();
-		initGridBagLayout(gblc3, 0, 2, 2, 3, 0.5, 0.6);
+		initGridBagLayout(gblc3, 0, 1, 1, 1, 0.25, 0.5);
 		GridBagConstraints gblc4 = new GridBagConstraints();
-		initGridBagLayout(gblc4, 2, 2, 2, 2, 0.4, 0.5);
+		initGridBagLayout(gblc4, 0, 2, 2, 3, 0.5, 0.6);
+		GridBagConstraints gblc5 = new GridBagConstraints();
+		initGridBagLayout(gblc5, 2, 2, 2, 2, 0.5, 0.4);
+		GridBagConstraints gblc6 = new GridBagConstraints();
+		initGridBagLayout(gblc6, 2, 1, 2, 2, 0.25, 0.2);
+
+		JButton button1 = new JButton("Boton 1");
+		JButton button2 = new JButton("Boton 2");
+		JButton button3 = new JButton("Boton 3");
+		JButton button4 = new JButton("Boton 4");
+		JButton button5 = new JButton("Boton 5");
+		JButton button6 = new JButton("Boton 6");
+
+		contentPane.add(button1, gblc1);
+		contentPane.add(button2, gblc2);
+		contentPane.add(button3, gblc3);
+		contentPane.add(button4, gblc4);
+		contentPane.add(button5, gblc5);
+		contentPane.add(button6, gblc6);
+
+		// MESA 1
+		ImageIcon mesa1 = new ImageIcon(classloader.getResource("mesa.png"));
+		mesa1.setImage(mesa1.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
+		button1.setIcon(mesa1);
+		button1.setHorizontalTextPosition(SwingConstants.CENTER);
+		button1.setVerticalTextPosition(SwingConstants.BOTTOM);
+
+		// MESA 2
+		ImageIcon mesa2 = new ImageIcon(classloader.getResource("mesa.png"));
+		mesa2.setImage(mesa2.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
+		button2.setIcon(mesa2);
+		button2.setHorizontalTextPosition(SwingConstants.CENTER);
+		button2.setVerticalTextPosition(SwingConstants.BOTTOM);
+
+		// MESA 3
+		ImageIcon mesa3 = new ImageIcon(classloader.getResource("mesa.png"));
+		mesa3.setImage(mesa1.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
+		button3.setIcon(mesa3);
+		button3.setHorizontalTextPosition(SwingConstants.CENTER);
+		button3.setVerticalTextPosition(SwingConstants.BOTTOM);
 		
+		// MESA 4
+		ImageIcon mesa4 = new ImageIcon(classloader.getResource("mesa.png"));
+		mesa4.setImage(mesa4.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
+		button4.setIcon(mesa4);
+		button4.setHorizontalTextPosition(SwingConstants.CENTER);
+		button4.setVerticalTextPosition(SwingConstants.BOTTOM);
 
-		JButton buttton1 = new JButton("Boton 1");
-		JButton buttton2 = new JButton("Boton 2");
-		JButton buttton3 = new JButton("Boton 3");
-		JButton buttton4 = new JButton("Boton 4");
-
-
+		// MESA 5
+		ImageIcon mesa5 = new ImageIcon(classloader.getResource("mesa.png"));
+		mesa5.setImage(mesa5.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
+		button5.setIcon(mesa5);
+		button5.setHorizontalTextPosition(SwingConstants.CENTER);
+		button5.setVerticalTextPosition(SwingConstants.BOTTOM);
 		
-
-		contentPane.add(buttton1, gblc1);
-		contentPane.add(buttton2, gblc2);
-		contentPane.add(buttton3, gblc3);
-		contentPane.add(buttton4, gblc4);}
+		// MESA 6
+		ImageIcon mesa6 = new ImageIcon(classloader.getResource("mesa.png"));
+		mesa6.setImage(mesa6.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
+		button6.setIcon(mesa6);
+		button6.setHorizontalTextPosition(SwingConstants.CENTER);
+		button6.setVerticalTextPosition(SwingConstants.BOTTOM);
+	}
 
 }
