6f41138317334f592528d8698b47214051eb6d77
Brayan <brayan8abo@outlook.es>
2024-10-18 17:22:05 +0200

    update

--- src/main/java/Actividad3/TPVview.java
+++ src/main/java/Actividad3/TPVview.java
@@ -15,6 +15,8 @@ import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Insets;
+import java.awt.CardLayout;
 
 public class TPVview extends JFrame {
 
@@ -38,14 +40,6 @@ public class TPVview extends JFrame {
 	}
 
 	public void initGridBagLayout(GridBagConstraints gblc, int x, int y, int w, int h, double wx, double wh) {
-
-		gblc.gridx = x;
-		gblc.gridy = y;
-		gblc.gridwidth = w;
-		gblc.gridheight = h;
-		gblc.weightx = wx;
-		gblc.weighty = wh;
-		gblc.fill = GridBagConstraints.BOTH;//
 	}
 
 	public TPVview() {
@@ -57,89 +51,113 @@ public class TPVview extends JFrame {
 		ClassLoader classloader = getClass().getClassLoader();
 
 		setContentPane(contentPane);
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		contentPane.setLayout(gbl_contentPane);
-
-		GridBagConstraints gblc1 = new GridBagConstraints();
-		initGridBagLayout(gblc1, 0, 0, 2, 1, 0.2, 0.5);
-		GridBagConstraints gblc2 = new GridBagConstraints();
-		initGridBagLayout(gblc2, 2, 0, 2, 1, 0.2, 0.5);
-		GridBagConstraints gblc3 = new GridBagConstraints();
-		initGridBagLayout(gblc3, 0, 1, 1, 1, 0.25, 0.5);
-		GridBagConstraints gblc4 = new GridBagConstraints();
-		initGridBagLayout(gblc4, 0, 2, 2, 3, 0.5, 0.6);
-		GridBagConstraints gblc5 = new GridBagConstraints();
-		initGridBagLayout(gblc5, 2, 2, 2, 2, 0.5, 0.4);
-		GridBagConstraints gblc6 = new GridBagConstraints();
-		initGridBagLayout(gblc6, 2, 1, 2, 2, 0.25, 0.2);
-
-		JButton button1 = new JButton("MESA 1");
-		JButton button2 = new JButton("MESA 2");
-		JButton button3 = new JButton("MESA 3");
-		JButton button4 = new JButton("MESA 4");
-		JButton button5 = new JButton("MESA 5");
-		JButton button6 = new JButton("MESA 6");
-		
-		button1.setFont(new Font("Linux Libertine G", Font.BOLD, 18));
-		button2.setFont(new Font("Linux Libertine G", Font.BOLD, 18));
-		button3.setFont(new Font("Linux Libertine G", Font.BOLD, 18));
-		button4.setFont(new Font("Linux Libertine G", Font.BOLD, 18));
-		button5.setFont(new Font("Linux Libertine G", Font.BOLD, 18));
-		button6.setFont(new Font("Linux Libertine G", Font.BOLD, 18));
-
-		contentPane.add(button1, gblc1);
-		contentPane.add(button2, gblc2);
-		contentPane.add(button3, gblc3);
-		contentPane.add(button4, gblc4);
-		contentPane.add(button5, gblc5);
-		contentPane.add(button6, gblc6);
+		CardLayout cardLayout = new CardLayout(0, 0);
+		contentPane.setLayout(cardLayout);
 
 		// MESA 1
-		ImageIcon mesa1 = new ImageIcon(classloader.getResource("mesa.png"));
-		mesa1.setImage(mesa1.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
-		button1.setIcon(mesa1);
-		button1.setHorizontalTextPosition(SwingConstants.CENTER);
+		ImageIcon iconoMesa = new ImageIcon(classloader.getResource("mesa.png"));
+		iconoMesa.setImage(iconoMesa.getImage().getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH));
+		
+//	TODO ESTO TE SOBRA
+//		// MESA 2
+//		ImageIcon mesa2 = new ImageIcon(classloader.getResource("mesa.png"));
+//		mesa2.setImage(mesa2.getImage().getScaledInstance(80, 80, java.awt.Image.SCALE_SMOOTH));
+//
+//		// MESA 3
+//		ImageIcon mesa3 = new ImageIcon(classloader.getResource("mesa.png"));
+//		mesa3.setImage(iconoMesa.getImage().getScaledInstance(80, 80, java.awt.Image.SCALE_SMOOTH));
+//		
+//		// MESA 4
+//		ImageIcon mesa4 = new ImageIcon(classloader.getResource("mesa.png"));
+//		mesa4.setImage(mesa4.getImage().getScaledInstance(80, 80, java.awt.Image.SCALE_SMOOTH));
+//
+//		// MESA 5
+//		ImageIcon mesa5 = new ImageIcon(classloader.getResource("mesa.png"));
+//		mesa5.setImage(mesa5.getImage().getScaledInstance(80, 80, java.awt.Image.SCALE_SMOOTH));
+//		
+//		// MESA 6
+//		ImageIcon mesa6 = new ImageIcon(classloader.getResource("mesa.png"));
+//		mesa6.setImage(mesa6.getImage().getScaledInstance(80, 80, java.awt.Image.SCALE_SMOOTH));
+//		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, "principal");
+		panel.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		//ayer vi una cosa de arrays de botones, para poner lo de los entrantes, primeros, segundos y esas cosas
+		//yo lo hice con un bucle de botones jajaj
+		//ahora si toco el boton 1 como hago o que hago para que me salga lo otro 
+		
+		JButton button1 = new JButton("MESA 1");
+		
 		button1.setVerticalTextPosition(SwingConstants.BOTTOM);
-
-		// MESA 2
-		ImageIcon mesa2 = new ImageIcon(classloader.getResource("mesa.png"));
-		mesa2.setImage(mesa2.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
-		button2.setIcon(mesa2);
-		button2.setHorizontalTextPosition(SwingConstants.CENTER);
+		button1.setHorizontalTextPosition(SwingConstants.CENTER);
+		button1.setFont(new Font("Linux Libertine G", Font.BOLD, 18));
+		button1.setIcon(iconoMesa);
+		panel.add(button1);
+		
+		JButton button2 = new JButton("MESA 2");
 		button2.setVerticalTextPosition(SwingConstants.BOTTOM);
-
-		// MESA 3
-		ImageIcon mesa3 = new ImageIcon(classloader.getResource("mesa.png"));
-		mesa3.setImage(mesa1.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
-		button3.setIcon(mesa3);
-		button3.setHorizontalTextPosition(SwingConstants.CENTER);
+		button2.setHorizontalTextPosition(SwingConstants.CENTER);
+		button2.setFont(new Font("Linux Libertine G", Font.BOLD, 18));
+		button2.setIcon(iconoMesa);
+		panel.add(button2);
+		
+		JButton button3 = new JButton("MESA 3");
 		button3.setVerticalTextPosition(SwingConstants.BOTTOM);
+		button3.setHorizontalTextPosition(SwingConstants.CENTER);
+		button3.setFont(new Font("Linux Libertine G", Font.BOLD, 18));
+		button3.setIcon(iconoMesa);
+		panel.add(button3);
 		
-		// MESA 4
-		ImageIcon mesa4 = new ImageIcon(classloader.getResource("mesa.png"));
-		mesa4.setImage(mesa4.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
-		button4.setIcon(mesa4);
-		button4.setHorizontalTextPosition(SwingConstants.CENTER);
+		JButton button4 = new JButton("MESA 4");
 		button4.setVerticalTextPosition(SwingConstants.BOTTOM);
-
-		// MESA 5
-		ImageIcon mesa5 = new ImageIcon(classloader.getResource("mesa.png"));
-		mesa5.setImage(mesa5.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
-		button5.setIcon(mesa5);
-		button5.setHorizontalTextPosition(SwingConstants.CENTER);
-		button5.setVerticalTextPosition(SwingConstants.BOTTOM);
+		button4.setHorizontalTextPosition(SwingConstants.CENTER);
+		button4.setFont(new Font("Linux Libertine G", Font.BOLD, 18));
+		button4.setIcon(iconoMesa);
+		panel.add(button4);
 		
-		// MESA 6
-		ImageIcon mesa6 = new ImageIcon(classloader.getResource("mesa.png"));
-		mesa6.setImage(mesa6.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH));
-		button6.setIcon(mesa6);
-		button6.setHorizontalTextPosition(SwingConstants.CENTER);
+		JButton button5 = new JButton("MESA 5");
+		button5.setVerticalTextPosition(SwingConstants.BOTTOM);
+		button5.setHorizontalTextPosition(SwingConstants.CENTER);
+		button5.setFont(new Font("Linux Libertine G", Font.BOLD, 18));
+		button5.setIcon(iconoMesa);
+		panel.add(button5);
+		
+		JButton button6 = new JButton("MESA 6");
 		button6.setVerticalTextPosition(SwingConstants.BOTTOM);
+		button6.setHorizontalTextPosition(SwingConstants.CENTER);
+		button6.setFont(new Font("Linux Libertine G", Font.BOLD, 18));
+		button6.setIcon(iconoMesa);
+		panel.add(button6);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, "ejemplo");
+		panel_1.setLayout(null);
+		setLocationRelativeTo(null);
+		
+		
+		
+		
+		//LISTENNERS DE BOTONES DE MESAS
+		button1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String botonesComanda[] = {"ENTRANTES","PRIMEROS","ENTRANTES","SEGUNDOS","POSTRES","BEBIDAS"};
+				JButton botones[] = new JButton[botonesComanda.length];
+				for (int i = 0; i < botones.length; i++) {
+					
+					botones[i]=new JButton(botonesComanda[i]);
+					botones[i].setVerticalTextPosition(SwingConstants.BOTTOM);
+					botones[i].setHorizontalTextPosition(SwingConstants.CENTER);
+					botones[i].setFont(new Font("Linux Libertine G", Font.BOLD, 18));
+					botones[i].setIcon(iconoMesa);
+					panel_1.add(botones[i]);
+					
+				}
+				cardLayout.show(contentPane, "ejemplo");
+				
+			}
+		});
 	}
 
-	protected JPanel JPanel() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
